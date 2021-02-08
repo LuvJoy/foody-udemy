f81af82f8653e25729cec7518947b438da3a4f95
Joseph <wjsdudtkd95@gmail.com>
2021-02-08 15:41:50 +0900

    [feat] Create Adapter
    - Apply DiffUtil

---
+++ app/src/main/java/com/joseph/foody/adapters/RecipesAdapter.kt
@@ -0,0 +1,52 @@
+package com.joseph.foody.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.joseph.foody.databinding.RecipesRowLayoutBinding
+import com.joseph.foody.models.FoodRecipe
+import com.joseph.foody.models.Result
+import com.joseph.foody.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+
+    fun setData(newData: FoodRecipe) {
+        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil,)
+        recipe = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
