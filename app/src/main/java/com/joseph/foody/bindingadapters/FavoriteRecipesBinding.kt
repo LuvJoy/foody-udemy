c4ef2ea294c96f80efbcd7c8747bd0b9e61c3595
Joseph <wjsdudtkd95@gmail.com>
2021-02-23 23:14:34 +0900

    [feat] Create favorite_recipes

---
+++ app/src/main/java/com/joseph/foody/bindingadapters/FavoriteRecipesBinding.kt
@@ -0,0 +1,43 @@
+package com.joseph.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.joseph.foody.adapters.FavoriteRecipesAdapter
+import com.joseph.foody.data.database.entities.FavoritesEntity
+
+@BindingAdapter("viewVisibility", "setData", requireAll = false)
+fun setDataAndViewVisibility(
+    view: View,
+    favoritesEntity: List<FavoritesEntity>?,
+    mAdapter: FavoriteRecipesAdapter?
+) {
+    if(favoritesEntity.isNullOrEmpty()) {
+        when(view) {
+            is ImageView -> {
+                view.visibility = View.VISIBLE
+            }
+            is TextView -> {
+                view.visibility = View.VISIBLE
+            }
+            is RecyclerView -> {
+                view.visibility = View.INVISIBLE
+            }
+        }
+    } else {
+        when(view) {
+            is ImageView -> {
+                view.visibility = View.INVISIBLE
+            }
+            is TextView -> {
+                view.visibility = View.INVISIBLE
+            }
+            is RecyclerView -> {
+                view.visibility = View.VISIBLE
+                mAdapter?.setData(favoritesEntity)
+            }
+        }
+    }
+}
--- app/src/main/java/com/joseph/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/joseph/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -1,11 +1,36 @@
 package com.joseph.foody.ui.fragments.favorites
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.joseph.foody.R
+import com.joseph.foody.adapters.FavoriteRecipesAdapter
 import com.joseph.foody.base.BaseFragment
 import com.joseph.foody.databinding.FragmentFavoriteRecipesBinding
+import com.joseph.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteRecipesFragment : BaseFragment<FragmentFavoriteRecipesBinding>(R.layout.fragment_favorite_recipes) {
 
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mainViewModel = mainViewModel
+        binding.mAdapter = mAdapter
+
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        binding.favoriteRecipesRecyclerView.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 }
